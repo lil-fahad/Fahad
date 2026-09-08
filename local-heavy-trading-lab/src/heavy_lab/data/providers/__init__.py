3236697e7d25ccb406d88a10dfd10791741efe5f
lil-fahad <73719703+lil-fahad@users.noreply.github.com>
2026-09-09 00:01:46 +0300

    feat: add market data provider interfaces

---
+++ local-heavy-trading-lab/src/heavy_lab/data/providers/__init__.py
@@ -0,0 +1,4 @@
+from heavy_lab.data.providers.base import MarketProvider
+from heavy_lab.data.providers.nasdaq import NasdaqBarsProvider, parse_bars
+
+__all__ = ["MarketProvider", "NasdaqBarsProvider", "parse_bars"]
